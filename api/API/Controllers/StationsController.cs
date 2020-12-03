17b69c9c522d566091cacc9c60dc3bab5227745e
Kiryl Volkau <volkaukiryl@gmail.com>
2020-12-03 14:10:31 +0300

    Changed to .NET 5.

--- api/API/Controllers/StationsController.cs
+++ api/API/Controllers/StationsController.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 namespace API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class StationController : ControllerBase
     {
         private readonly StationDao _dao;
